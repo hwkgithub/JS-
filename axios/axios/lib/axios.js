1668318cb9176c394ea603c63ec1bcd9406e50c1
hwkgithub <2965498304@qq.com>
2020-06-03 22:58:22 +0800

    axios学习

--- axios/axios/lib/axios.js
+++ axios/axios/lib/axios.js
@@ -1,3 +1,10 @@
+/*
+ * @Description: 
+ * @Autor: HWK
+ * @Date: 2020-05-31 21:05:57
+ * @LastEditors: HWK
+ * @LastEditTime: 2020-06-03 22:57:03
+ */ 
 'use strict';
 
 var utils = require('./utils');
@@ -13,24 +20,24 @@ var defaults = require('./defaults');
  * @return {Axios} A new instance of Axios
  */
 function createInstance(defaultConfig) {
-  /* 
-  创建Axios的实例
-      原型对象上有一些用来发请求的方法: get()/post()/put()/delete()/request()
-      自身上有2个重要属性: defaults/interceptors
-  */  
-  var context = new Axios(defaultConfig);
+    /* 
+    创建Axios的实例
+        原型对象上有一些用来发请求的方法: get()/post()/put()/delete()/request()
+        自身上有2个重要属性: defaults/interceptors
+    */
+    var context = new Axios(defaultConfig);
 
-  // axios和axios.create()对应的就是request函数
-  // Axios.prototype.request.bind(context)
-  var instance = bind(Axios.prototype.request, context); // axios
+    // axios和axios.create()对应的就是request函数
+    // Axios.prototype.request.bind(context)
+    var instance = bind(Axios.prototype.request, context); // axios
 
-  // 将Axios原型对象上的方法拷贝到instance上: request()/get()/post()/put()/delete()
-  utils.extend(instance, Axios.prototype, context); 
+    // 将Axios原型对象上的方法拷贝到instance上: request()/get()/post()/put()/delete()
+    utils.extend(instance, Axios.prototype, context);
 
-  // 将Axios实例对象上的属性拷贝到instance上: defaults和interceptors属性
-  utils.extend(instance, context);
+    // 将Axios实例对象上的属性拷贝到instance上: defaults和interceptors属性
+    utils.extend(instance, context);
 
-  return instance;
+    return instance;
 }
 
 // Create the default instance to be exported
@@ -41,7 +48,7 @@ axios.Axios = Axios;
 
 // Factory for creating new instances
 axios.create = function create(instanceConfig) {
-  return createInstance(mergeConfig(axios.defaults, instanceConfig));
+    return createInstance(mergeConfig(axios.defaults, instanceConfig));
 };
 
 // Expose Cancel & CancelToken
@@ -51,7 +58,7 @@ axios.isCancel = require('./cancel/isCancel');
 
 // Expose all/spread
 axios.all = function all(promises) {
-  return Promise.all(promises);
+    return Promise.all(promises);
 };
 axios.spread = require('./helpers/spread');
 
