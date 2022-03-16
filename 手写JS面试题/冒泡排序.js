9fc0c65d32cf8111fc71135968ac78fe594826e2
hwkyeon <2965498304@qq.com>
2022-03-16 17:33:37 +0800

    * 新增 数组去重

--- 手写JS面试题/冒泡排序.js
+++ 手写JS面试题/冒泡排序.js
@@ -3,22 +3,22 @@
  * @Autor: HWK
  * @Date: 2022-03-16 16:03:09
  * @LastEditors: HWK
- * @LastEditTime: 2022-03-16 16:17:18
+ * @LastEditTime: 2022-03-16 17:00:19
  */
-const array = [1, 4, -8, -3, 6, 12, 9, 8];
+var array = [1, 4, -8, -3, 6, 12, 9, 8];
 
 function sort(arr) {
-    let temp;
     for (let i = 0; i < arr.length - 1; i++) {
         for (let j = 0; i < arr.length - 1 - i; j++) {
             if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
+                let temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
         }
     }
+    return arr
 }
 
-sort(array);
-console.log(array);
+let arr = sort(array);
+console.log(arr);
--- 手写JS面试题/数组去重.js
+++ 手写JS面试题/数组去重.js
@@ -3,5 +3,21 @@
  * @Autor: HWK
  * @Date: 2022-03-16 16:16:54
  * @LastEditors: HWK
- * @LastEditTime: 2022-03-16 16:16:54
+ * @LastEditTime: 2022-03-16 17:33:11
  */
+let arr = [1, 23, 1, 1, 1, 3, 23, 5, 6, 7, 9, 9, 8, 5, 5, 5, 5];
+
+function noRepeat(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                arr.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return arr
+}
+
+let arr2 = noRepeat(arr);
+console.log(arr2);
