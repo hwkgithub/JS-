e8edb7024e6c6495fc359279846eee217b8c072f
hwkyeon <2965498304@qq.com>
2022-07-21 21:33:38 +0800

    * 新增 数组去重方法

--- 手写JS面试题/数组去重.js
+++ 手写JS面试题/数组去重.js
@@ -5,9 +5,15 @@
  * @LastEditors: HWK
  * @LastEditTime: 2022-03-16 17:33:11
  */
-let arr = [1, 23, 1, 1, 1, 3, 23, 5, 6, 7, 9, 9, 8, 5, 5, 5, 5];
 
-function noRepeat(arr) {
+/** 
+ * @type {*} 
+ * splice方法
+ * 会让数据的索引变化，不是最优解
+ * */
+let arr1 = [1, 23, 1, 1, 1, 3, 23, 5, 6, 7, 9, 9, 8, 5, 5, 5, 5];
+
+function noRepeat1(arr) {
     for (let i = 0; i < arr.length - 1; i++) {
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[i] === arr[j]) {
@@ -19,5 +25,36 @@ function noRepeat(arr) {
     return arr
 }
 
-let arr2 = noRepeat(arr);
-console.log(arr2);
+let newArr1 = noRepeat1(arr1);
+console.log(newArr1);
+
+/** 
+ * @type {*} 
+ * filter方法
+ * 
+ * */
+let arr2 = ['apple', 'banana', 'pear', 'apple', 'orange', 'orange'];
+
+let newArr2 = arr2.filter(function (value, index, arr) {
+    return arr.indexOf(value) === index;
+});
+
+console.log(newArr2);    //["apple", "banana", "pear", "orange"]
+
+/** 
+ * @type {*} 
+ * ES6 Set()
+ * 
+ * */
+let arr3 = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 4, 3, 2, 1, 1, 1];
+
+function noRepeat2(arr3) {
+    let newArr = [];
+    let myset = new Set(arr3);//利用了Set结构不能接收重复数据的特点
+    for (var val of myset) {
+        newArr.push(val)
+    }
+    return newArr;
+}
+var newArr3 = noRepeat2(arr3)
+console.log(newArr3);    //[1, 2, 3, 4, 5]
